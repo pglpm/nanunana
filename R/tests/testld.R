10a66d7f2cce0c6897b37fb427ab92d95199a9c5
luca <piergianluca@gmail.com>
2017-09-21 11:08:02 +0200

    update manuscript

--- R/tests/testld.R
+++ R/tests/testld.R
@@ -2,8 +2,9 @@ library('magicaxis')
 library('mvtnorm')
 library('ellipse')
 library('LaplacesDemon')
+library('RColorBrewer')
 
-mymu <- 1
+mymu <- 3
 mysigma <- 5
 
 draws <- rnorm(40,mymu,mysigma)
@@ -12,7 +13,7 @@ print(mean(draws))
 
 print(sd(draws)*(n-1)/n)
 
-Data <- list(data=draws, mon.names='', parm.names=c('mu','sigma'),N=n)
+Data <- list(data=draws, mon.names='predictive distr.', parm.names=c('mu','sigma'),N=n)
 
 Model=function(parm,Data){
     parm[2] <- interval(parm[2],1e-100,100)
@@ -20,28 +21,30 @@ Model=function(parm,Data){
     sigma.prior <- dhalfcauchy(parm[2], 25, log=TRUE)
     LL <- sum(dnorm(Data$data, parm[1], parm[2], log=TRUE))
     LP <- LL + mu.prior + sigma.prior
-    return=list(LP=LP,Dev=2*LL,Monitor=1,yhat=rnorm(1,parm[1],parm[2]),parm=parm)}
+    return=list(LP=LP,Dev=2*LL,Monitor=rnorm(1,parm[1],parm[2]),yhat=rnorm(1,parm[1],parm[2]),parm=parm)}
 
-FitCoin2D <- LaplacesDemon(Model, Data, Initial.Values=c(0,1),
+Fit <- LaplacesDemon(Model, Data, Initial.Values=c(0,1),
                         Covar=NULL, Iterations=10000, Status=1000, Thinning=1,
-                        Algorithm="AFSS",
+                        CPUs=2, Algorithm="AFSS",
                         Specs=list(A=500, B=NULL, m=100, n=0, w=1))
 
 png('testplot.png')
-magcon(FitCoin2D$Posterior2[9000:10000,1],FitCoin2D$Posterior2[9000:10000,2],
-       conlevels=c(0.5,0.68,0.95), lty=c(2,1,3))
+magcon(Fit$Posterior2[9000:10000,1],Fit$Posterior2[9000:10000,2],
+       conlevels=c(0.5,0.68,0.95), lty=c(2,1,3), imcol=brewer.pal(n=9,name='Blues'))
 
 title(xlab='mu',ylab='sigma')
 
-polygon(ellipse(cov.rob(FitCoin2D$Posterior2)$cov,centre=FitCoin2D$Summary2[,'Mean'],
+polygon(ellipse(cov.rob(Fit$Posterior2)$cov,centre=Fit$Summary2[,'Mean'],
                 level=c(pnorm(1)-pnorm(-1))), col=hsv(v=0,alpha=0.2),border=NA)
 
 points(mymu,mysigma,col='blue',pch=4)
 
-points(FitCoin2D$Summary2[1,'Median'],FitCoin2D$Summary2[2,'Median'],
+points(Fit$Summary2[1,'Median'],Fit$Summary2[2,'Median'],
 col='black',pch=4)
 dev.off()
 
 png('testplottrajectory.png')
-magplot(FitCoin2D$Posterior1,type='l',col=hsv(alpha=0.3),xlab='mu',ylab='sigma')
+magplot(Fit$Posterior1,type='l',col=hsv(alpha=0.3),xlab='mu',ylab='sigma')
 dev.off()
+
+plot(Fit,BurnIn=500,Data,PDF=TRUE,Parms=NULL)
