ad071ba7c8e61cb0fa1a71f85014d4174a581c42
luca <piergianluca@gmail.com>
2017-09-21 10:25:21 +0200

    update manuscript

--- R/tests/testld.R
+++ R/tests/testld.R
@@ -3,17 +3,24 @@ library('mvtnorm')
 library('ellipse')
 library('LaplacesDemon')
 
-draws <- rnorm(40,1,2)
+mymu <- 1
+mysigma <- 5
+
+draws <- rnorm(40,mymu,mysigma)
+n <- length(draws)
 print(mean(draws))
-Data <- list(data=draws, mon.names='', parm.names=c('mu','sigma'),N=length(draws))
+
+print(sd(draws)*(n-1)/n)
+
+Data <- list(data=draws, mon.names='', parm.names=c('mu','sigma'),N=n)
 
 Model=function(parm,Data){
     parm[2] <- interval(parm[2],1e-100,100)
-    mu.prior <- dnorm(parm[1], 0,1000, log=TRUE)
+    mu.prior <- dnorm(parm[1], 0, 1000, log=TRUE)
     sigma.prior <- dhalfcauchy(parm[2], 25, log=TRUE)
-    LL <- sum(dnorm(Data$data,parm[1],parm[2], log=TRUE))
+    LL <- sum(dnorm(Data$data, parm[1], parm[2], log=TRUE))
     LP <- LL + mu.prior + sigma.prior
-    return=list(LP=LP,Dev=2*LL,Monitor=1,yhat=1,parm=parm)}
+    return=list(LP=LP,Dev=2*LL,Monitor=1,yhat=rnorm(1,parm[1],parm[2]),parm=parm)}
 
 FitCoin2D <- LaplacesDemon(Model, Data, Initial.Values=c(0,1),
                         Covar=NULL, Iterations=10000, Status=1000, Thinning=1,
@@ -29,8 +36,12 @@ title(xlab='mu',ylab='sigma')
 polygon(ellipse(cov.rob(FitCoin2D$Posterior2)$cov,centre=FitCoin2D$Summary2[,'Mean'],
                 level=c(pnorm(1)-pnorm(-1))), col=hsv(v=0,alpha=0.2),border=NA)
 
-points(1,2,col='blue',pch=4)
+points(mymu,mysigma,col='blue',pch=4)
 
 points(FitCoin2D$Summary2[1,'Median'],FitCoin2D$Summary2[2,'Median'],
-col='red',pch=4)
+col='black',pch=4)
+dev.off()
+
+png('testplottrajectory.png')
+magplot(FitCoin2D$Posterior1,type='l',col=hsv(alpha=0.3),xlab='mu',ylab='sigma')
 dev.off()
