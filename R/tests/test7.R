b9b7278223a46e09044b2d165baaa69f64c7ca6d
luca <piergianluca@gmail.com>
2017-09-24 02:24:09 +0200

    update manuscript

---
+++ R/tests/test7.R
@@ -0,0 +1,221 @@
+## test script for normal model with normal-Wishart prior (cf minka1998_r2001)
+
+library('magicaxis')
+library('ellipse')
+library('MCMCpack')
+library('LaplacesDemon')
+library('RColorBrewer')
+library('mvtnorm')
+library('magrittr')
+
+## density-plot function
+densplot <- function (x,adjust=1,...) { density(x,adjust) %>% plot(.,...)}
+
+set.seed(666)
+
+## parameters to generate normal training data
+d <- 2
+mud <- c(1,2)
+sigmad <- matrix(c(2,1,1,4), 2)
+
+## training data with means and scatter matrix
+nt <- 3
+datat <- rmvnorm(nt,mud,sigmad)
+meant <- colMeans(datat)
+hmeant <- sweep(datat,2,meant,'-')
+scattermt <- t(hmeant) %*% hmeant
+
+## reference-prior parameters (mean and variance matrix)
+n0 <- 5
+mean0 <- c(0,0)
+scatterm0 <- diag(c(1,1))
+
+## combination of training data and reference parameters
+
+nt0 <- nt + n0
+meant0 <- (nt*meant + n0*mean0)/nt0
+scattermt0 <- (nt*scattermt + n0*scatterm0)/nt0 +
+    nt*n0*((meant-mean0) %*% t(meant-mean0))/nt0^2
+
+## extra datum
+datanew <- rmvnorm(1,mud,sigmad)
+
+## exact results:
+
+## P(datanew | datat, prior) is t-student
+pnew.exact <- mvtnorm::dmvt(datanew, delta=meant0, sigma=scattermt0*(nt0+1)/(nt0-d+1), df=nt0-d+1, type='shifted',log=F)
+
+PGF <- function(data){
+    sigma <- rinvwishart(n0,scatterm0*n0)
+    mu <- rmvnorm(1,mean=mean0,sigma=sigma/n0)
+    return(c(mu,c(sigma)[c(1,2,4)]))
+}
+
+mydata <- list(data=datat, predict=datanew, PGF=PGF, mon.names=c('P(D_0)','D0[1]','D0[2]'), parm.names=c('mu[1]','mu[2]','sigma[1,1]','sigma[1,2]','sigma[2,2]'),N=nt, y=meant)
+
+mydata0 <- list(data=datat, predict=datanew, PGF=PGF, mon.names=c('P(D)'), parm.names=c('mu[1]','mu[2]','sigma[1,1]','sigma[1,2]','sigma[2,1]','sigma[2,2]'),N=nt, y=meant)
+
+hyperprior <- function(parm,data){
+    mu <- parm[1:2]
+    parm[3] <- interval(parm[3],1e-6,Inf)
+    parm[5] <- interval(parm[5],1e-6,Inf)
+    parm[4] <- interval(parm[4]/sqrt(parm[3]*parm[5]),-1,1)*sqrt(parm[3]*parm[5])
+    sigma <- matrix(c(parm[c(3,4,4,5)]), 2)
+    sigma.prior <- dinvwishart(sigma,n0,scatterm0*n0, log=T)
+    mu.prior <- dmvnorm(mu,mean=mean0,sigma=sigma/n0, log=T)
+    LL <- sum(dmvnorm(data$data,mean=mu,sigma=sigma,log=T))
+    LP <- LL + mu.prior + sigma.prior
+    return <- list(LP=LP, Dev=-2*LL,
+                   Monitor=c(dmvnorm(data$predict,mean=mu,sigma=sigma),rmvnorm(1,mean=mu,sigma=sigma)),
+                  yhat=rmvnorm(1,mean=mu,sigma=sigma),parm=parm)
+}
+
+hyperprior0 <- function(parm,data){
+    parm.prior <- dnormv(parm,mu0,sigma0, log=T)
+    LP <- parm.prior
+    return <- list(LP=LP, Dev=-2*LP, Monitor=exp(sum(dnormv(data$data,parm,sigma,log=T))) ,yhat=rnormv(1,parm,sigma),parm=parm)
+}
+
+posterior <- function(parm,data){
+    LP <- 1
+    return <- list(LP=LP,Dev=1,Monitor=1,yhat=rnormv(1,parm,sigma),parm=parm)
+}
+
+Initial.Values <- c(0,0,1,0,1)
+Sample <- LaplacesDemon(hyperprior, mydata, Initial.Values,
+                        Covar=NULL,
+                        Thinning=10,
+                        Iterations=10000, Status=1000,
+                        Algorithm="AFSS",
+                        Specs=list(A=1000, B=NULL, m=100, n=0, w=1)
+                        )
+
+plot(Sample,BurnIn=500,mydata,PDF=TRUE,Parms=NULL)
+
+stop()
+#postpredictive <- predict(Sample,posterior,mydata,CPUs=2)
+
+## png('testnormal_normal.png')
+## densplot(postpredictive$yhat,
+##     adjust=0.1,
+##     main='test normal-normal',
+##     xlab=expression('mu'))
+## xx <- seq(mun-3*sigmat,mun+3*sigmat,0.1)
+## yy <- dnormv(xx,mun,sigmat)
+## lines(x=xx,y=yy,col='red')
+## dev.off()
+
+png('predictive_probability.png')
+exactpdata <- dnormv(mydata$predict,mun,sigmat)
+pdata <- mean(Sample$Monitor[,1])
+densplot(Sample$Monitor[,1],
+    adjust=0.002,
+    main='predictive probability for data_0 + uncertainty',
+    xlab=paste('P(data_0 = ',draws0,' | data_training) = ',pdata,' (red = est., blue = exact)'),
+    ylab='p(P)')
+abline(v=pdata,col='red')
+abline(v=exactpdata,col='blue')
+dev.off()
+
+png('predictive_posterior.png')
+densplot(Sample$Monitor[,2],
+    adjust=0.1,
+    main='predictive posterior distribution',
+    xlab=expression('data_0 (blue = exact distr)'),
+    ylab=expression('P(data_0 | data_training)'))
+xx <- seq(mun-3*sigmat,mun+3*sigmat,0.1)
+yy <- dnormv(xx,mun,sigmat)
+lines(x=xx,y=yy,col='blue')
+dev.off()
+
+Sample0 <- LaplacesDemon(hyperprior0, mydata0, Initial.Values=c(1), Thinning=2,
+                     Iterations=100000, Status=10000,
+                     Algorithm="AFSS",
+                     Specs=list(A=500, B=NULL, m=100, n=0, w=1)
+                     )
+
+
+png('model_probability.png')
+exactp <- sqrt(sigma)/sqrt((2*pi*sigma)^n*(n*sigma0+sigma))*exp(
+-sum(draws^2)/(2*sigma)-mu0^2/(2*sigma0)+(sigma0*n^2*me^2/sigma + sigma*mu0^2/sigma0 + 2*n*me*mu0)/(2*(n*sigma0+sigma)))
+pmodel <- mean(Sample0$Monitor[,1])
+densplot(Sample0$Monitor[,1],
+    adjust=max(Sample0$Monitor[,1])/50,
+    main='probability of model + uncertainty',
+    xlab=paste('P(model | data) = ',pmodel,' (red=est., blue=exact)'))
+abline(v=pmodel,col='red')
+abline(v=exactp,col='blue')
+dev.off()
+
+plot(Sample0,BurnIn=500,mydata0,PDF=TRUE,Parms=NULL)
+
+
+
+stop()
+
+
+#### garbage ####
+
+
+xgrid <- seq(meant0[1]-1*scattermt0[1,1],meant0[1]+1*scattermt0[1,1],length.out=20)
+ygrid <- seq(meant0[2]-1*scattermt0[2,2],meant0[2]+1*scattermt0[2,2],length.out=20)
+tgrid <- expand.grid(x=xgrid,y=ygrid)
+values <- mvtnorm::dmvt(as.matrix(tgrid),delta=(meant0),sigma=scattermt0*(nt0+1)/(nt0-d+1), df=nt0-d+1, type='shifted',log=F)
+png('contour.png')
+contour(x = xgrid, y = ygrid, z = matrix(values, nrow=20, ncol=20),nlevels=50)
+dev.off()
+
+sum(exp(-(mu0-draws)^2/(2*(sigma+sigma0))))/sqrt(2*pi*(sigma+sigma0))
+
+
+                                       
+
+
+
+len <- length(Sample0$Posterior2)
+summ <-0
+for(i in 1:len){
+summ <- summ + prod(len*dnormv(draws,Sample0$Posterior2[i],sigma))
+}
+summ/len^length(draws)/len
+
+
+densplot(Sample$Monitor,
+    adjust=1,
+    main='test',
+    xlab='tau')
+
+xx <- seq(-8,8,0.1)
+yy <- dt.scaled(xx,df,mean=mu,sd=sd)
+lines(x=xx,y=yy,col='red')
+
+
+quadr <- IterativeQuadrature(hyperprior, c(5), mydata, Covar=NULL, Iterations=500,
+                             sir=F, Algorithm="AGHSG",
+                             Specs=list(K=5, Kmax=7, Packages=NULL, Dyn.libs=NULL),
+                             Stop.Tolerance=c(1e-5,1e-15), CPUs=1)
+
+
+
+
+stop()
+
+## png('testplot2.png')
+## magcon(Fit$Posterior2[9000:10000,1],Fit$Posterior2[9000:10000,2],
+##        conlevels=c(0.5,0.68,0.95), lty=c(2,1,3), imcol=brewer.pal(n=9,name='Blues'))
+
+## title(xlab='mu',ylab='sigma')
+
+## polygon(ellipse(cov.rob(Fit$Posterior2)$cov,centre=Fit$Summary2[,'Mean'],
+##                 level=c(pnorm(1)-pnorm(-1))), col=hsv(v=0,alpha=0.2),border=NA)
+
+## points(mymu,mysigma,col='blue',pch=4)
+
+## points(Fit$Summary2[1,'Median'],Fit$Summary2[2,'Median'],
+## col='black',pch=4)
+## dev.off()
+
+## png('testplottrajectory2.png')
+## magplot(Fit$Posterior1,type='l',col=hsv(alpha=0.3),xlab='mu',ylab='sigma')
+## dev.off()
+
